5db8bc7e6b1dc2c27372a04710acd024b5e172a1
SteamB23 <steamb23@outlook.com>
2022-06-07 14:47:44 +0900

    초기 커밋

---
+++ PGChatBot/Program.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using static System.Console;
+using static System.Threading.Thread;
+
+const string userId = "steamb23@outlook.com";
+const string password = "~;5(Vg^t;X61r}D";
+const string uid = "487";
+var texts = new[]
+{
+    "햄스터김치볶음",
+    "$$볼보자동차",
+    "끼에에엑"
+};
+
+
+using var driver = new EdgeDriver()
+{
+    Url = "https://chat.zniq.co/",
+};
+
+// 로그인
+WriteLine("Login...");
+var loginIdInput = driver.FindElement(By.Id("login-id"));
+var loginPasswordInput = driver.FindElement(By.Id("login-password"));
+
+loginIdInput.SendKeys(userId);
+loginPasswordInput.SendKeys(password);
+loginPasswordInput.SendKeys(Keys.Enter);
+Sleep(100); // 갱신 대기
+
+// 마지막 채팅 체크
+WriteLine("Message duplicate test...");
+var chatMessages = driver.FindElements(By.ClassName("chatMessage"));
+var lastChatMessage = chatMessages.Last();
+var lastChatName = lastChatMessage.FindElement(By.ClassName("chatName"));
+if (lastChatName.GetAttribute("title") == uid)
+{
+    var lastChatText = lastChatMessage.FindElement(By.ClassName("chatText"));
+
+    if (texts.Any(text => lastChatText.Text.Contains(text)))
+    {
+        WriteLine("Still the same message remains. Terminate jobs to avoid duplication.");
+        return;
+    }
+}
+
+// 메시지 입력 테스트
+WriteLine("MessageBox enter...");
+var messagebox = driver.FindElement(By.Id("message-text"));
+
+messagebox.SendKeys(texts[Random.Shared.Next(0, texts.Length)]);
+messagebox.SendKeys(Keys.Enter);
